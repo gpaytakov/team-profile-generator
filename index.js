aaee09d18add5a8a5c2fcf7770c349627fc38968
gpay <gpaytakov@gmail.com>
2022-04-17 13:47:26 -0500

    generates website

--- index.js
+++ index.js
@@ -1,213 +1,214 @@
+// question asking and collecting answers module is imported
 const inquirer = require('inquirer');
+
+// file system module is called
+const fs = require('fs');
+
+// team member class creation files imported
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
-const generatePage = require('./src/page-template');
-const generateSite = require('./utils/generate-site.js');
 
+// website creation file is linked
+const generateCards = require('./src/generateSite');
+
+// employees array is created and set to empty array, after creating employee profile we will add to it
 const employees = [];
 
+// a function checks for if id input is positive integer or not, returns boolean
 function isPositiveInteger(id) {
     return id >>> 0 === parseFloat(id);
 }
 
-function addManager() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: "What is the Employee's name? [Letters only] (Required)",
-                validate: nameInput => {
-                    if (/^[a-z]+$/i.test(nameInput)) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter Employee name! [Letters only]');
-                        return false;
-                    }
+const addManager = () => {
+return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Who is the Manager of the team? [Letters only] (Required)",
+            validate: nameInput => {
+                if (/^[a-z]+$/i.test(nameInput)) {
+                    return true;
+                } else {
+                    console.log("\nPlease enter Manager's name! [Letters only]");
+                    return false;
                 }
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "What is the Employee's id #? (Required)",
-                validate: id => {
-                    if (isPositiveInteger(id) > 0) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter positive integer!')
-                        return false;
-                    }
-                } 
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "What is the Employee's email address? [sample@example.xyz](Required)",
-                validate: function(email) {
-                    // Regex mail check (return true if valid mail)
-                    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
-                }
-            },
-            {
-                type: 'input',
-                name: 'office',
-                message: "What is the Manager's office number? (Required)"
             }
-
-        ])
-        .then((answers) => {
-            
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
-            employees.push([manager.name, manager.id, manager.email, manager.office, manager.getRole()]);
-            menu();
-        });
-}
-
-function addEngineer() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: "What is the Employee's name? [Letters only] (Required)",
-                validate: nameInput => {
-                    if (/^[a-z]+$/i.test(nameInput)) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter Employee name! [Letters only]');
-                        return false;
-                    }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the Manager's id #? (Required)",
+            validate: id => {
+                if (isPositiveInteger(id) > 0) {
+                    return true;
+                } else {
+                    console.log('\nPlease enter positive integer!')
+                    return false;
                 }
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "What is the Employee's id #? (Required)",
-                validate: id => {
-                    if (isPositiveInteger(id) > 0) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter positive integer!')
-                        return false;
-                    }
-                } 
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "What is the Employee's email address? [sample@example.xyz](Required)",
-                validate: function(email) {
-                    // Regex mail check (return true if valid mail)
-                    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+            } 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Manager's email address? [sample@example.xyz](Required)",
+            validate: function(email) {
+                // Regex mail check (return true if valid mail)
+                isValid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                if (isValid) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid email!');
+                    return false;
                 }
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: "What is the Engineer's github username? (Required)"
             }
-
-        ])
-        .then((answers) => {
-            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-            employees.push([engineer.name, engineer.id, engineer.email, engineer.github, engineer.getRole()]);
-            menu();
-        });
-}
-
-function addIntern() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: "What is the Employee's name? [Letters only] (Required)",
-                validate: nameInput => {
-                    if (/^[a-z]+$/i.test(nameInput)) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter Employee name! [Letters only]');
-                        return false;
-                    }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the Manager's office number? (Required)",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ('Please enter an office number! [Positive integer]')
+                    return false; 
+                } else {
+                    return true;
                 }
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: "What is the Employee's id #? (Required)",
-                validate: id => {
-                    if (isPositiveInteger(id) > 0) {
-                        return true;
-                    } else {
-                        console.log('\nPlease enter positive integer!')
-                        return false;
-                    }
-                } 
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "What is the Employee's email address? [sample@example.xyz](Required)",
-                validate: function(email) {
-                    // Regex mail check (return true if valid mail)
-                    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+            }
+        }
+
+    ])
+    .then(managerInput => {
+        const manager = new Manager(managerInput.name, managerInput.id, managerInput.email, managerInput.office);
+        employees.push(manager);
+    })
+};
+
+const addEmployee = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Please select employee role',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name? [Letters only] (Required)",
+            validate: nameInput => {
+                if (/^[a-z]+$/i.test(nameInput)) {
+                    return true;
+                } else {
+                    console.log('\nPlease enter employee name! [Letters only]');
+                    return false;
                 }
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: 'What school intern attends? (Required)'
             }
-
-        ])
-        .then((answers) => {
-            
-            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-            employees.push([intern.name, intern.id, intern.email, intern.school, intern.getRole()]);
-            menu();
-        });
-}
-
-function menu() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'choice',
-                message: 'What you want to do?',
-                choices: ['add Engineer', 'add Intern', 'add Manager', 'build a Team']
-            }    
-        ])
-        .then((answers) => {
-            console.log(answers);
-            const choice = answers.choice;
-            switch (choice) {
-                case 'add Manager':
-                    addManager();
-                    break;
-                case 'add Engineer':
-                    addEngineer();
-                    break;
-                case 'add Intern':
-                    addIntern();
-                    break;
-                case 'build a Team':
-                    buildTeam(employees);
-                    return;
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the Employee's id #? (Required)",
+            validate: id => {
+                if (isPositiveInteger(id) > 0) {
+                    return true;
+                } else {
+                    console.log('\nPlease enter positive integer!')
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Employee's email address? [sample@example.xyz](Required)",
+            validate: function(email) {
+                // Regex mail check (return true if valid mail)
+                isValid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                if (isValid) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid email!');
+                    return false;
+                }
             }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the Engineer's github username? (Required)",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's github username!")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school intern attends? (Required)',
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Please enter the Intern's school!")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add another Employee?',
+            default: false
+        }
 
-        })
-    
+    ])
+    .then(dataForEmployee => {
+        let {name, id, email, role, github, school, confirmAddEmployee} = dataForEmployee
+        let employee;
+        if (role === 'Engineer') {
+            employee = new Engineer(name, id, email, github);
+        } else if (role === 'Intern') {
+            employee = new Intern(name, id, email, school);
+        }
+
+        employees.push(employee);
+        
+        if (confirmAddEmployee) {
+            return addEmployee(employee);
+        } else {
+            console.log(employees);
+            return employees;
+        }
+        
+    });
 }
 
-function buildTeam (employees) {
-    console.log('You have completed building your Team!');
-    console.log(employees);
-    
-    // employees.forEach(employee => {
-    //     console.log(employee);
-    // })
-}
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Your page is successfully created! Please see the index.html file in dist folder')
+        }
+    })
+};
 
-menu();
+addManager()
+    .then(addEmployee)
+    .then(employees => {
+        return  generateCards(employees);
+    })
+    .then(htmlPage => {
+        return writeFile(htmlPage);
+    })
+    .catch(err => {
+        console.log(err);
+    })
---
+++ src/generateSite.js
@@ -0,0 +1,114 @@
+const generateManager = manager => {
+    return `
+    <div>
+        <article class='card'>
+            <div class='card-header'>
+                <h3>${manager.name}</h3>
+                <h4>${manager.getRole()}</h4>
+            </div>
+            <div class='card-content'>
+                <p class='id'>ID: ${manager.id}</p>
+                <p class='email'>Email: <a href='mailto:${manager.email}'>${manager.email}</a></p>
+                <p class='office'>Office #: ${manager.office}</p>
+            </div>
+        </article>
+    </div>
+    `;
+}
+
+const generateEngineer = engineer => {
+    return `
+    <div>
+        <article class='card'>
+            <div class='card-header'>
+                <h3>${engineer.name}</h3>
+                <h4>${engineer.getRole()}</h4>
+            </div>
+            <div class='card-content'>
+                <p class='id'>ID: ${engineer.id}</p>
+                <p class='email'>Email: <a href='mailto:${engineer.email}'>${engineer.email}</a></p>
+                <p class='github'>Github: <a href='https://github.com/${engineer.github}'>${engineer.github}</a></p>
+            </div>
+        </article>
+    </div>
+    `;    
+}
+
+const generateIntern = intern => {
+    return `
+    <div>
+        <article class='card'>
+            <div class='card-header'>
+                <h3>${intern.name}</h3>
+                <h4>${intern.getRole()}</h4>
+            </div>
+            <div class='card-content'>
+                <p class='id'>ID: ${intern.id}</p>
+                <p class='email'>Email: <a href='mailto:${intern.email}'>${intern.email}</a></p>
+                <p class='school'>School: ${intern.school}</p>
+            </div>
+        </article>
+    </div>
+    `;
+}
+
+generateCards = data => {
+    arr = [];
+    for (let i=0; i<data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+            arr.push(managerCard);
+        }
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+            arr.push(engineerCard);
+        }
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+            arr.push(internCard);
+        }
+    }
+    const employeeCards = arr.join('')
+    const buildTeam = buildTeamPage(employeeCards);
+    return buildTeam;
+    
+}
+
+const buildTeamPage = employeeCards => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+  
+    <body>
+        <header>
+            <nav class='navbar' id='navbar'>
+                <h1 class="page-title">Team Members</h1>
+            </nav>
+        </header>
+        <main>
+            <div class='container'> 
+                <div>
+                    ${employeeCards}
+                <div>
+            </div>
+        </main>
+        <footer>
+            <h3 class='footer'>&copy; ${new Date().getFullYear()} by Guvanchmyrat Paytakov</h3>
+        </footer>
+    </body>
+  </html>
+  `;
+}
+
+// exports module to index.js
+module.exports = generateCards;
